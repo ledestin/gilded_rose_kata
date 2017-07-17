d4481661d9c4edac3a453d9262ef873681b9486d
Dmitry Maksyoma <ledestin@gmail.com>
2017-07-18 06:45:58 +1200

    Test refactor

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,13 +1,13 @@
 require_relative 'gilded_rose'
 
 describe "update_quality" do
-  let(:regular_item) { Item.new 'Mirror', 3, 20 }
-
   context "regular item" do
+    let(:item) { Item.new 'Mirror', 3, 20 }
+
     it "sell_in and quality decrease by 1" do
-      tick regular_item
-      expect(regular_item.sell_in).to eq 2
-      expect(regular_item.quality).to eq 19
+      tick item
+      expect(item.sell_in).to eq 2
+      expect(item.quality).to eq 19
     end
   end
 end
